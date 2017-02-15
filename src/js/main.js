1cb3fd310ef1104e9e7a14501a30d491b7ab9e7f
Chris Coralli <ccoralli@gmail.com>
2017-02-15 11:07:29 -0500

    minor styling, auto wait+retry to grab data.

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,36 @@
 import $ from 'jquery'
 
 var BASE_URL = "https://api.github.com"
+var retryCount = 0;
 
-function getRepoStats (event)  {
-  event.preventDefault();
-
+function fetchData()  {
   var user = $("#user-name").val();
   var repo = $("#repo-name").val();
 
   $.ajax({
     url: `${BASE_URL}/repos/${user}/${repo}/stats/contributors`,
     dataType: "json",
-    success: function (data, status, request) {
-      if (request.status === 202) {
-        //tell user to wait
-        //reschedule request on timeout
-      } else {
-        console.log(data);
-      };
-    }
+    success: displayStats
   });
+}
+
+function displayStats (data, status, request) {
+  if (request.status === 202) {
+    var messages = $("messages");
+    retryCount++;
+    messages.empty();
+    messages.append(`<p>This is retry number ${retryCount}.</p>`)
+    messages.append("<p>The data for that project is being processed.</p>");
+    messages.append("<p>We will resend your request in 60 second</p>");
+    setTimeout(function() {fetchData(); }, 60000);
+  } else {
+    console.log(data);
+  };
+}
+
+function getRepoStats (event)  {
+  event.preventDefault();
+  fetchData();
 }
 
 var form = $("form");

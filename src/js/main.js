686bfaf54d3056f9ce20ca31819a2d26454f4fdc
Chris Coralli <ccoralli@gmail.com>
2017-02-15 10:00:46 -0500

    basic fetch.

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+import $ from 'jquery'
+
+var BASE_URL = "https://api.github.com"
+
+function getRepoStats (event)  {
+  event.preventDefault();
+
+  var user = $("#user-name").val();
+  var repo = $("#repo-name").val();
+
+  $.ajax({
+    url: `${BASE_URL}/repos/${user}/${repo}/stats/contributors`,
+    dataType: "json",
+    sucess: function (data) {
+      console.log(data);
+    }
+  });
+}
+
+var form = $("form");
+
+form.submit(getRepoStats);
